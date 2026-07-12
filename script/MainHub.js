7e3510e7ad7024f69dd593fedb9b4191153f1be4
WangWengPlayz <labacowanwan@gmail.com>
2026-07-12 21:42:05 +0000

    Update

--- script/MainHub.js
+++
@@ -1,8 +0,0 @@
--- @tags: hub, main, universal
--- MainHub - example script served by MJL HUB
-print("MainHub loaded!")
-
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-print("Welcome, " .. player.Name .. "! MainHub is ready.")
